da03e0c6ce8530e1b3424fe5dde4761271056b45
mitko17 <mitkodim739@gmail.com>
2021-03-28 16:27:40 +0300

    st

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 50;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -145,8 +145,8 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+static unsigned int cols = 65;
+static unsigned int rows = 20;
 
 /*
  * Default colour and shape of the mouse cursor
